5a16b20f882dea534fe355a357b87acbd795d51f
Dawitlior <dawitlior777@gmail.com>
2023-04-03 13:37:29 +0300

    compo order

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,9 @@ const App = () => {
           <Hero />
         </div>
         <About />
-        <Experience />
         <Tech />
         <Works />
+        <Experience />
         {/* <Feedbacks /> */}
         <div className='relative z-0'>
           <Contact />
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915EFF]'>Lior</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I Full-stack developer, experience <br className='sm:block hidden' />
+            A Full-stack developer, experience in <br className='sm:block hidden' />
             building web applications
           </p>
         </div>
